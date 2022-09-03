d1bcec6c645b1e807b7bd595a2e15a1c46c1a396
SerhiiSamara <ndaair@gmail.com>
2022-09-03 23:20:01 +0300

    Розкоментував

--- type1.js
+++ type1.js
@@ -6,62 +6,62 @@
 
 // https://www.codewars.com/kata/577e9095d648a15b800000d4/train/javascript
 
-// function postfixEvaluator(string) {
-//   let total;
-//   const arr = string.split(" ");
-//   console.log(arr);
+function postfixEvaluator(string) {
+  let total;
+  const arr = string.split(" ");
+  console.log(arr);
 
-//   if (arr.length === 1) {
-//     total = arr[0];
-//     console.log(total);
-//     return;
-//   }
+  if (arr.length === 1) {
+    total = arr[0];
+    console.log(total);
+    return;
+  }
 
-//   if (arr.length === 2) {
-//     console.log("Invalid subscription type");
-//   }
+  if (arr.length === 2) {
+    console.log("Invalid subscription type");
+  }
 
-//   for (let i = 0; i <= arr.length - 1; i += 1) {
-//     if (i < -1) {
-//       return;
-//     }
+  for (let i = 0; i <= arr.length - 1; i += 1) {
+    if (i < -1) {
+      return;
+    }
 
-//     let a = Number(arr[i]);
-//     let b = Number(arr[i + 1]);
-//     let c = !Number(arr[i + 2]);
+    let a = Number(arr[i]);
+    let b = Number(arr[i + 1]);
+    let c = !Number(arr[i + 2]);
 
-//     if (a && b && c) {
-//       switch (arr[i + 2]) {
-//         case "+":
-//           total = a + b;
-//           break;
+    if (a && b && c) {
+      switch (arr[i + 2]) {
+        case "+":
+          total = a + b;
+          break;
 
-//         case "-":
-//           total = a - b;
-//           break;
+        case "-":
+          total = a - b;
+          break;
 
-//         case "*":
-//           total = a * b;
-//           break;
+        case "*":
+          total = a * b;
+          break;
 
-//         case "/":
-//           total = (a - (a % b)) / b;
-//           break;
+        case "/":
+          total = (a - (a % b)) / b;
+          break;
 
-//         default:
-//           console.log("Invalid subscription type");
-//       }
+        default:
+          console.log("Invalid subscription type");
+      }
 
-//       arr[i] = total;
-//       arr.splice(i + 1, 2);
-//       i -= 2;
-//     }
-//   }
-//   console.log(total);
+      arr[i] = total;
+      arr.splice(i + 1, 2);
+      i -= 2;
+    }
+  }
+  console.log(total);
 
-//   return total;
-// }
+  return total;
+}
 
-// postfixEvaluator("-1");
+postfixEvaluator("-1");
 
 // -------------------Не сошлись два ответа. Не понял их результат.------------------------
