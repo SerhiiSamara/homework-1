44b8fec175f22cc4dc66eea123dfddcc7c330548
SerhiiSamara <ndaair@gmail.com>
2022-10-13 19:05:07 +0300

    Set

---
+++ type4.js
@@ -0,0 +1,13 @@
+// function Dog(name, breed, color, sex) {
+//   this.name = name;
+//   this.breed = breed;
+//   this.color = color;
+//   this.sex = sex;
+// }
+
+// const theDog = new Dog("Болтушка", "лабрадор", "шоколадный", "девочка");
+
+// console.log(theDog.toString()); // вернёт [object Object]
+// console.log(theDog);
+
+console.log(Math.random());
---
+++ type5.js
@@ -0,0 +1,29 @@
+const dog = '{"name":"Mango"}';
+
+const dogJSON = JSON.parse(dog);
+console.log(dogJSON);
+
+// const json = '{"name":"Mango","age":3,"isHappy":true}';
+
+// const dog = JSON.parse(json);
+// console.log(dog); // {name: "Mango", age: 3, isHappy: true}
+// console.log(dog.name); // "Mango"
+
+// burn out
+// Dariy M14:04
+// Fraud - шахрай
+// phony -фальшивий
+// counterfiet
+// Make up for - компенсувати
+// make up - створити або помиритись
+// make for -напрвлятись
+// Make up for - компенсувати
+// make up - створити або помиритись
+// make for -напрвлятись
+// crash = fall asleep
+//flaws  невдачі
+// disadvantage - неперевага
+// should have been better
+// Monday through Friday
+// Caused to призвело
+// resulted to
